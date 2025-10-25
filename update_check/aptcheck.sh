fb0120c6c0aee68c7f2fb550c89b6656c68c753f
bkosciow <kosci1@gmail.com>
2025-10-25 20:18:23 +0200

    updates

---
+++ update_check/aptcheck.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Requires package "recode" for proper output of flatpak list
+# Capture the list of upgraded packages and flatpaks
+apt_upgrades=$(apt-get -s upgrade | grep '^Inst' | awk '{print $2}' | tr '[:upper:]' '[:lower:]' | sort | uniq)
+
+Url="https://cloud.bkosciow.dev/ocs/v2.php/apps/spreed/api/v1/chat/kubi5p4r"
+User="MrBot"
+AccessToken="sfPBd-xwgbC-WRoqm-PLHHt-kZAGg"
+
+
+if [[ -n "$apt_upgrades" ]]; then
+    body="The following apt packages have upgrades:"
+    body+=$'\n\n'
+    body+="$apt_upgrades"
+    body+=$'\n\n'
+
+    echo "$body"
+
+JSON_PAYLOAD=$(jq -n --arg msg "$body" '{"message": $msg}')
+
+echo $JSON_PAYLOAD
+
+  curl -u ${User}:${AccessToken} -o /dev/null ${CurlArgs} \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H "OCS-APIRequest: true" \
+  --data "${JSON_PAYLOAD}" \
+  -X POST ${Url} 
+  
+fi
+
